04c84d6dcc6cf8e638b98e431d9cbcd2b3464f59
radenmasabdul <abdul.tamsis@gmail.com>
2023-01-19 02:18:10 +0700

    Setup API Laporan

--- backend/controllers/Laporan.js
+++ backend/controllers/Laporan.js
@@ -1,21 +1,160 @@
-import Laporan from "../models/LaporanModel.js";
+import Laporan from "../models/LaporanModel.js"
+import Users from "../models/UsersModel.js";
+import { Op } from "sequelize";
 
-export const getLaporan = (req, res) => {
-
+export const getLaporan = async (req, res) => {
+    try {
+        let response;
+        if (req.role === "admin") {
+            response = await Laporan.findAll({
+                attributes: ['uuid', 'name', 'price'],
+                include: [{
+                    model: Users,
+                    attributes: ['name', 'email']
+                }]
+            });
+        } else if (req.role === "userHQ") {
+            response = await Laporan.findAll({
+                attributes: ['uuid', 'name', 'price'],
+                where: {
+                    userId: req.userId
+                },
+                include: [{
+                    model: Users,
+                    attributes: ['name', 'email']
+                }]
+            });
+        } else {
+            if (Users.role !== "admin") return res.status(403).json({ msg: "Akses Terlarang" });
+        }
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ msg: error.messages })
+    }
 }
 
-export const getLaporanById = (req, res) => {
-
+export const getLaporanById = async (req, res) => {
+    try {
+        const laporan = await Laporan.findOne({
+            where: {
+                uuid: req.params.id
+            }
+        });
+        if (!laporan) res.status(404).json({ msg: "Data Tidak Ditemukan" });
+        let response;
+        if (req.role === "admin") {
+            response = await Laporan.findOne({
+                attributes: ['uuid', 'name', 'price'],
+                where: {
+                    id: laporan.id
+                },
+                include: [{
+                    model: Users,
+                    attributes: ['name', 'email']
+                }]
+            });
+        } else if (req.role === "userHQ") {
+            response = await Laporan.findOne({
+                attributes: ['uuid', 'name', 'price'],
+                where: {
+                    [Op.and]: [{ id: laporan.id }, { userId: req.userId }]
+                },
+                include: [{
+                    model: Users,
+                    attributes: ['name', 'email']
+                }]
+            });
+        } else {
+            if (Users.role !== "admin") return res.status(403).json({ msg: "Akses Terlarang" });
+        }
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ msg: error.messages })
+    }
 }
 
-export const createLaporan = (req, res) => {
-
+export const createLaporan = async (req, res) => {
+    const { name, price } = req.body;
+    try {
+        if (req.role === "admin") {
+            await Laporan.create({
+                name: name,
+                price: price,
+                userId: req.userId
+            });
+        } else if (req.role === "userHQ") {
+            await Laporan.create({
+                name: name,
+                price: price,
+                userId: req.userId
+            });
+        } else {
+            if (Users.role !== "admin") return res.status(403).json({ msg: "Akses Terlarang" });
+        }
+        res.status(201).json({ msg: "Laporan Berhasil Dibuat" });
+    } catch (error) {
+        res.status(500).json({ msg: error.messages })
+    }
 }
 
-export const updateLaporan = (req, res) => {
-
+export const updateLaporan = async (req, res) => {
+    try {
+        const laporan = await Laporan.findOne({
+            where: {
+                uuid: req.params.id
+            }
+        });
+        if (!laporan) res.status(404).json({ msg: "Data Tidak Ditemukan" });
+        const { name, price } = req.body;
+        if (req.role === "admin") {
+            await Laporan.update({ name, price }, {
+                where: {
+                    id: laporan.id
+                }
+            })
+        } else if (req.role === "userHQ") {
+            if (req.userId !== laporan.userId) return res.status(403).json({ msg: "Akses Terlarang" });
+            await Laporan.update({ name, price }, {
+                where: {
+                    [Op.and]: [{ id: laporan.id }, { userId: req.userId }]
+                }
+            })
+        } else {
+            if (Users.role !== "admin") return res.status(403).json({ msg: "Akses Terlarang" });
+        }
+        res.status(200).json({ msg: "Produk Berhasil Diperbaharui" });
+    } catch (error) {
+        res.status(500).json({ msg: error.messages })
+    }
 }
 
-export const deleteLaporan = (req, res) => {
-
+export const deleteLaporan = async (req, res) => {
+    try {
+        const laporan = await Laporan.findOne({
+            where: {
+                uuid: req.params.id
+            }
+        });
+        if (!laporan) res.status(404).json({ msg: "Data Tidak Ditemukan" });
+        const { name, price } = req.body;
+        if (req.role === "admin") {
+            await Laporan.destroy({
+                where: {
+                    id: laporan.id
+                }
+            })
+        } else if (req.role === "userHQ") {
+            if (req.userId !== laporan.userId) return res.status(403).json({ msg: "Akses Terlarang" });
+            await Laporan.destroy({
+                where: {
+                    [Op.and]: [{ id: laporan.id }, { userId: req.userId }]
+                }
+            })
+        } else {
+            if (Users.role !== "admin") return res.status(403).json({ msg: "Akses Terlarang" });
+        }
+        res.status(200).json({ msg: "Produk Berhasil Dihapus" });
+    } catch (error) {
+        res.status(500).json({ msg: error.messages })
+    }
 }
--- backend/routes/LaporanRoutes.js
+++ backend/routes/LaporanRoutes.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { getLaporan, getLaporanById, createLaporan, updateLaporan, deleteLaporan } from "../controllers/Laporan.js";
+import { verifyUser } from "../middleware/AuthUser.js"
 
 const router = express.Router();
 
-router.get('/laporan', getLaporan);
-router.get('/laporan/:id', getLaporanById);
-router.post('/laporan', createLaporan);
-router.patch('/laporan/:id', updateLaporan);
-router.delete('/laporan/:id', deleteLaporan);
+router.get('/laporan', verifyUser, getLaporan);
+router.get('/laporan/:id', verifyUser, getLaporanById);
+router.post('/laporan', verifyUser, createLaporan);
+router.patch('/laporan/:id', verifyUser, updateLaporan);
+router.delete('/laporan/:id', verifyUser, deleteLaporan);
 
 export default router;
