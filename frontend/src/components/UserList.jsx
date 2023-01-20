f8dd7438716450d36feea019341435771d821e7f
radenmasabdul <abdul.tamsis@gmail.com>
2023-01-21 01:46:31 +0700

    Get & Delete User

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const response = await axios.get("http://localhost:5000/users");
+    setUsers(response.data);
+  };
+
+  const deleteUser = async (userId) => {
+    await axios.delete(`http://localhost:5000/users/${userId}`);
+    getUsers();
+  };
+
   return (
     <>
       <div className="container mt-12">
+        <Link to="/users/add" className="btn btn-success my-2 text-white">
+          {" "}
+          Add New
+        </Link>
         <div className="overflow-x-auto">
           <table className="table table-zebra table-compact w-full">
             <thead>
@@ -16,13 +38,30 @@ const UserList = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th>1</th>
-                <td>Abdul Rahman Alhafizh</td>
-                <td>abdul.tamsis@gmail.com</td>
-                <td>Admin</td>
-                <td></td>
-              </tr>
+              {users.map((user, index) => (
+                <tr key={user.uuid} className="text-center">
+                  <th>{index + 1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.role}</td>
+                  <td className="flex flex-wrap gap-2 justify-center">
+                    <Link
+                      to={`/users/edit/${user.uuid}`}
+                      className="btn btn-info text-white"
+                    >
+                      {" "}
+                      Edit{" "}
+                    </Link>
+                    <button
+                      onClick={() => deleteUser(user.uuid)}
+                      className="btn btn-error text-white"
+                    >
+                      {" "}
+                      Delete{" "}
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
