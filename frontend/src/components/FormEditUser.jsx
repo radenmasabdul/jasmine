36e1d054ec9d0445f27bf459e6f67f5f76385f13
radenmasabdul <abdul.tamsis@gmail.com>
2023-01-21 02:27:23 +0700

    Update User

--- frontend/src/components/FormEditUser.jsx
+++ frontend/src/components/FormEditUser.jsx
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams, Link } from "react-router-dom";
 
 const FormEditUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [role, setRole] = useState("Admin");
+  const [message, setMessage] = useState("");
+  const { id } = useParams();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUserById = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/users/${id}`);
+        setName(response.data.name);
+        setEmail(response.data.email);
+        setRole(response.data.role);
+      } catch (error) {
+        if (error.message) {
+          setMessage(error.response.data.message);
+        }
+      }
+    };
+    getUserById();
+  }, [id]);
+
+  const updateUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.patch(`http://localhost:5000/users/${id}`, {
+        name: name,
+        email: email,
+        password: password,
+        confPassword: confPassword,
+        role: role,
+      });
+      navigate("/users");
+    } catch (error) {
+      if (error.response) {
+        setMessage(error.response.data.message);
+      }
+    }
+  };
+
   return (
     <>
       <div className="container mx-auto mt-12">
         <div className="card w-96 bg-base-100 shadow-xl">
-          <form className="my-10 mx-10">
+          <form className="my-10 mx-10" onSubmit={updateUser}>
+            <p className="text-center">{message}</p>
             <div className="relative z-0 w-full group mb-6">
               <input
                 type="text"
@@ -14,6 +61,8 @@ const FormEditUser = () => {
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=""
                 required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
               <label
                 for="floating_name"
@@ -30,6 +79,8 @@ const FormEditUser = () => {
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=""
                 required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <label
                 for="floating_email"
@@ -45,7 +96,8 @@ const FormEditUser = () => {
                 id="floating_password"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=""
-                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
               <label
                 for="floating_password"
@@ -61,7 +113,8 @@ const FormEditUser = () => {
                 id="floating_confirm_password"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=""
-                required
+                value={confPassword}
+                onChange={(e) => setConfPassword(e.target.value)}
               />
               <label
                 for="floating_confirm_password"
@@ -77,20 +130,33 @@ const FormEditUser = () => {
               Role
             </label>
             <select
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
               id="role"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <option>Admin</option>
-              <option>User HQ</option>
-              <option>Branch User</option>
+              <option disabled value="role" className="capitalize my-4">
+                select role
+              </option>
+              <option value="admin">Admin</option>
+              <option value="userHQ">User HQ</option>
+              <option value="branchUser">Branch User</option>
             </select>
-
-            <button
-              type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Update
-            </button>
+            <div className="flex flex-wrap gap-2 justify-left my-2">
+              <button
+                type="submit"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Update
+              </button>
+              <Link
+                to="/laporan"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                {" "}
+                Back{" "}
+              </Link>
+            </div>
           </form>
         </div>
       </div>
